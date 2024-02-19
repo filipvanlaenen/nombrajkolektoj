6586c239335aad43c4988e9b8c336b6b8523d86c
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-02-19 07:36:14 +0100

    Updated the POM file to depend on Kolektoj and added an empty class

---
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/NumericCollection.java
@@ -0,0 +1,11 @@
+package net.filipvanlaenen.nombrajkolektoj;
+
+import net.filipvanlaenen.kolektoj.Collection;
+
+/**
+ * Interface defining the signature for all numeric collections.
+ *
+ * @param <E> The element type.
+ */
+public interface NumericCollection<E> extends Collection<E> {
+}
---
+++ src/test/java/net/filipvanlaenen/nombrajkolektoj/NumericCollectionTest.java
@@ -0,0 +1,7 @@
+package net.filipvanlaenen.nombrajkolektoj;
+
+/**
+ * Unit tests on the {@link net.filipvanlaenen.nombrajkolektoj.NumericCollection} class.
+ */
+public class NumericCollectionTest {
+}

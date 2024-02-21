e2729cad7f3cf505db98e7d50c69f3c89aa4fe44
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-02-21 11:44:36 +0100

    Issue #6: Added unit tests to the LongCollection class

--- src/main/java/net/filipvanlaenen/nombrajkolektoj/longs/LongCollection.java
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/longs/LongCollection.java
@@ -6,56 +6,126 @@ import java.util.Spliterator;
 import net.filipvanlaenen.kolektoj.Collection;
 import net.filipvanlaenen.nombrajkolektoj.NumericCollection;
 
+/**
+ * An abstract class implementing the {@link net.filipvanlaenen.nombrajkolektoj.NumericCollection} interface for longs
+ * and containing inner classes with concrete implementations.
+ */
 public abstract class LongCollection extends AbstractLongCollection implements NumericCollection<Long> {
-    public final static class ArrayCollection extends LongCollection {
-        public ArrayCollection(Long... integers) {
-            super(new net.filipvanlaenen.kolektoj.array.ArrayCollection<Long>(integers));
+    /**
+     * Inner class using an array backed implementation of the {@link net.filipvanlaenen.kolektoj.Collection} interface.
+     */
+    public static final class ArrayCollection extends LongCollection {
+        /**
+         * Constructs a collection from another collection, with the same longs and the same element cardinality.
+         *
+         * @param source The collection to create a new collection from.
+         */
+        public ArrayCollection(final Collection<Long> source) {
+            this(source.getElementCardinality(), source.toArray());
+        }
+
+        /**
+         * Constructs a collection with the given longs and element cardinality.
+         *
+         * @param elementCardinality The element cardinality.
+         * @param longs              The longs of the collection.
+         */
+        public ArrayCollection(final ElementCardinality elementCardinality, final Long... longs) {
+            super(new net.filipvanlaenen.kolektoj.array.ArrayCollection<Long>(elementCardinality, longs));
+        }
+
+        /**
+         * Constructs a collection with the given longs. The element cardinality is defaulted to
+         * <code>DUPLICATE_ELEMENTS</code>.
+         *
+         * @param longs The longs of the collection.
+         */
+        public ArrayCollection(final Long... longs) {
+            super(new net.filipvanlaenen.kolektoj.array.ArrayCollection<Long>(longs));
         }
     }
 
-    private final Collection<Long> collection;
+    /**
+     * The collection holding the longs.
+     */
+    private final Collection<Long> longs;
 
-    public LongCollection(Collection<Long> collection) {
-        this.collection = collection;
+    /**
+     * Private constructor taking a collection with the longs as its parameter.
+     *
+     * @param longs The collection holding the longs.
+     */
+    private LongCollection(final Collection<Long> longs) {
+        this.longs = longs;
     }
 
     @Override
-    public boolean contains(Long arg0) {
-        return collection.contains(arg0);
+    public boolean contains(final Long element) {
+        return longs.contains(element);
     }
 
     @Override
-    public boolean containsAll(Collection<?> arg0) {
-        return collection.containsAll(arg0);
+    public boolean containsAll(final Collection<?> collection) {
+        return longs.containsAll(collection);
+    }
+
+    /**
+     * Returns a new empty longs collection.
+     *
+     * @return A new empty longs collection.
+     */
+    static LongCollection empty() {
+        return new ArrayCollection();
     }
 
     @Override
     public Long get() throws IndexOutOfBoundsException {
-        return collection.get();
+        return longs.get();
     }
 
     @Override
     public ElementCardinality getElementCardinality() {
-        return collection.getElementCardinality();
+        return longs.getElementCardinality();
     }
 
     @Override
     public Iterator<Long> iterator() {
-        return collection.iterator();
+        return longs.iterator();
+    }
+
+    /**
+     * Returns a new longs collection with the specified longs.
+     *
+     * @param longs The longs for the new long collection.
+     * @return A new longs collection with the specified longs.
+     */
+    static LongCollection of(final Long... longs) {
+        return new ArrayCollection(longs);
+    }
+
+    /**
+     * Returns a new longs collection with the specified element cardinality and the longs.
+     *
+     * @param elementCardinality The element cardinality.
+     * @param longs              The longs for the new longs collection.
+     * @return A new longs collection with the specified element cardinality and the longs.
+     */
+    static LongCollection of(final ElementCardinality elementCardinality, final Long... longs) {
+        return new ArrayCollection(elementCardinality, longs);
     }
 
     @Override
     public int size() {
-        return collection.size();
+        return longs.size();
     }
 
     @Override
     public Spliterator<Long> spliterator() {
-        return collection.spliterator();
+        return longs.spliterator();
     }
 
     @Override
     public Long[] toArray() {
-        return collection.toArray();
+        return longs.toArray();
     }
 }

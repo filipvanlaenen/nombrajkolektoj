3dab6e1f9dd71539c99a2e044464a63f99cc495a
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-02-21 12:08:51 +0100

    Issue #3: Defined the ModifiableNumericCollection

--- src/main/java/net/filipvanlaenen/nombrajkolektoj/ModifiableNumericCollection.java
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/ModifiableNumericCollection.java
@@ -2,8 +2,26 @@ package net.filipvanlaenen.nombrajkolektoj;
 
 import net.filipvanlaenen.kolektoj.ModifiableCollection;
 
-public interface ModifiableNumericCollection<E extends Number> extends NumericCollection<E>, ModifiableCollection<E> {
-    boolean multiply(E value);
+/**
+ * Interface defining the signature for all modifiable numeric collections.
+ *
+ * @param <N> The element type.
+ */
+public interface ModifiableNumericCollection<N extends Number> extends NumericCollection<N>, ModifiableCollection<N> {
+    /**
+     * Multiplies all the numbers in the collection with the multiplicand and returns whether any of the numbers were
+     * changed. Ignores <code>null</code> elements.
+     *
+     * @param multiplicand The number by which all numbers in the collection should be multiplied.
+     * @return True if any of the numbers in the collection was changed.
+     */
+    boolean multiply(N multiplicand);
 
+    /**
+     * Negates all the numbers in the collection and returns whether any of the numbers were changed. Ignores
+     * <code>null</code> elements.
+     *
+     * @return True if any of the numbers in the collection was changed.
+     */
     boolean negate();
 }

9c89d15497d32a74376bb036963e2910a3f7d7ac
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-02-21 09:44:02 +0100

    Issue #4: Resolved Checkstyle violations

--- src/main/java/net/filipvanlaenen/nombrajkolektoj/integers/IntegerCollection.java
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/integers/IntegerCollection.java
@@ -6,56 +6,96 @@ import java.util.Spliterator;
 import net.filipvanlaenen.kolektoj.Collection;
 import net.filipvanlaenen.nombrajkolektoj.NumericCollection;
 
+/**
+ * An abstract class implementing the {@link net.filipvanlaenen.nombrajkolektoj.NumericCollection} interface for
+ * integers and containing inner classes with concrete implementations.
+ */
 public abstract class IntegerCollection extends AbstractIntegerCollection implements NumericCollection<Integer> {
-    public final static class ArrayCollection extends IntegerCollection {
-        public ArrayCollection(Integer... integers) {
+    /**
+     * Inner class using an array backed implementation of the {@link net.filipvanlaenen.kolektoj.Collection} interface.
+     */
+    public static final class ArrayCollection extends IntegerCollection {
+        /**
+         * Constructs a collection from another collection, with the same integers and the same element cardinality.
+         *
+         * @param source The collection to create a new collection from.
+         */
+        public ArrayCollection(final Collection<Integer> source) {
+            this(source.getElementCardinality(), source.toArray());
+        }
+
+        /**
+         * Constructs a collection with the given integers and element cardinality.
+         *
+         * @param elementCardinality The element cardinality.
+         * @param integers           The integers of the collection.
+         */
+        public ArrayCollection(final ElementCardinality elementCardinality, final Integer... integers) {
+            super(new net.filipvanlaenen.kolektoj.array.ArrayCollection<Integer>(elementCardinality, integers));
+        }
+
+        /**
+         * Constructs a collection with the given integers. The element cardinality is defaulted to
+         * <code>DUPLICATE_ELEMENTS</code>.
+         *
+         * @param integers The integers of the collection.
+         */
+        public ArrayCollection(final Integer... integers) {
             super(new net.filipvanlaenen.kolektoj.array.ArrayCollection<Integer>(integers));
         }
     }
 
-    private final Collection<Integer> collection;
+    /**
+     * The collection holding the integers.
+     */
+    private final Collection<Integer> integers;
 
-    public IntegerCollection(Collection<Integer> collection) {
-        this.collection = collection;
+    /**
+     * Private constructor taking a collection with the integers as its parameter.
+     *
+     * @param integers The collection holding the integers.
+     */
+    private IntegerCollection(final Collection<Integer> integers) {
+        this.integers = integers;
     }
 
     @Override
-    public boolean contains(Integer arg0) {
-        return collection.contains(arg0);
+    public boolean contains(final Integer element) {
+        return integers.contains(element);
     }
 
     @Override
-    public boolean containsAll(Collection<?> arg0) {
-        return collection.containsAll(arg0);
+    public boolean containsAll(final Collection<?> collection) {
+        return collection.containsAll(collection);
     }
 
     @Override
     public Integer get() throws IndexOutOfBoundsException {
-        return collection.get();
+        return integers.get();
     }
 
     @Override
     public ElementCardinality getElementCardinality() {
-        return collection.getElementCardinality();
+        return integers.getElementCardinality();
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return collection.iterator();
+        return integers.iterator();
     }
 
     @Override
     public int size() {
-        return collection.size();
+        return integers.size();
     }
 
     @Override
     public Spliterator<Integer> spliterator() {
-        return collection.spliterator();
+        return integers.spliterator();
     }
 
     @Override
     public Integer[] toArray() {
-        return collection.toArray();
+        return integers.toArray();
     }
 }

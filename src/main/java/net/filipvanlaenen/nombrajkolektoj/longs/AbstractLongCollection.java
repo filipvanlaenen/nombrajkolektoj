235263f2ec67809e429e20c87e244e27fd9704df
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-02-27 18:05:43 +0100

    Issue #6: Added unit tests on AbstractLongCollection

--- src/main/java/net/filipvanlaenen/nombrajkolektoj/longs/AbstractLongCollection.java
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/longs/AbstractLongCollection.java
@@ -2,19 +2,21 @@ package net.filipvanlaenen.nombrajkolektoj.longs;
 
 import net.filipvanlaenen.nombrajkolektoj.NumericCollection;
 
+/**
+ * An abstract class implementing the methods defined in the
+ * {@link net.filipvanlaenen.nombrajkolektoj.NumericCollection} for longs.
+ */
 abstract class AbstractLongCollection implements NumericCollection<Long> {
     @Override
     public Long max() {
         if (size() == 0) {
-            return null;
+            throw new IndexOutOfBoundsException("Cannot return a maximum for an empty collection.");
         }
-        Long max = get();
-        for (Long i : this) {
-            if (i == null) {
-                return null;
-            }
-            if (i > max) {
-                max = i;
+        Long max = null;
+        for (Long l : this) {
+            // EQMU: Changing the conditional boundary below produces an equivalent mutant.
+            if (l != null && (max == null || l > max)) {
+                max = l;
             }
         }
         return max;
@@ -23,15 +25,13 @@ abstract class AbstractLongCollection implements NumericCollection<Long> {
     @Override
     public Long min() {
         if (size() == 0) {
-            return null;
+            throw new IndexOutOfBoundsException("Cannot return a minimum for an empty collection.");
         }
         Long min = get();
-        for (Long i : this) {
-            if (i == null) {
-                return null;
-            }
-            if (i < min) {
-                min = i;
+        for (Long l : this) {
+            // EQMU: Changing the conditional boundary below produces an equivalent mutant.
+            if (l != null && (min == null || l < min)) {
+                min = l;
             }
         }
         return min;
@@ -40,11 +40,10 @@ abstract class AbstractLongCollection implements NumericCollection<Long> {
     @Override
     public Long product() {
         Long product = 1L;
-        for (Long i : this) {
-            if (i == null) {
-                return null;
+        for (Long l : this) {
+            if (l != null) {
+                product *= l;
             }
-            product *= i;
         }
         return product;
     }
@@ -52,11 +51,10 @@ abstract class AbstractLongCollection implements NumericCollection<Long> {
     @Override
     public Long sum() {
         Long sum = 0L;
-        for (Long i : this) {
-            if (i == null) {
-                return null;
+        for (Long l : this) {
+            if (l != null) {
+                sum += l;
             }
-            sum += i;
         }
         return sum;
     }

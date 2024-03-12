f30f2d39da2e89d2f3a39503342a38cb54add595
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-03-12 09:29:39 +0100

    Issue #16: Defined the UpdatableSortedNumericMap interface

---
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/UpdatableSortedNumericMap.java
@@ -0,0 +1,13 @@
+package net.filipvanlaenen.nombrajkolektoj;
+
+import net.filipvanlaenen.kolektoj.UpdatableSortedMap;
+
+/**
+ * Interface defining the signature for all updatable sorted numeric maps.
+ * 
+ * @param <K> The key type.
+ * @param <N> The value type.
+ */
+public interface UpdatableSortedNumericMap<K, N extends Number>
+        extends UpdatableNumericMap<K, N>, UpdatableSortedMap<K, N> {
+}

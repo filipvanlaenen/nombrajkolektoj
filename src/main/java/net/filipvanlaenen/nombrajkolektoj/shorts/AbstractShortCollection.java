4d366667204b4aceb50a3d264edfa2e030a9e6fb
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-04-02 21:19:44 +0200

    Issue #24: Started to implement the ShortCollection class

---
+++ src/main/java/net/filipvanlaenen/nombrajkolektoj/shorts/AbstractShortCollection.java
@@ -0,0 +1,61 @@
+package net.filipvanlaenen.nombrajkolektoj.shorts;
+
+import net.filipvanlaenen.nombrajkolektoj.NumericCollection;
+
+/**
+ * An abstract class implementing the methods defined in the
+ * {@link net.filipvanlaenen.nombrajkolektoj.NumericCollection} for shorts.
+ */
+abstract class AbstractShortCollection implements NumericCollection<Short> {
+    @Override
+    public Short max() throws IndexOutOfBoundsException {
+        if (size() == 0) {
+            throw new IndexOutOfBoundsException("Cannot return a maximum for an empty collection.");
+        }
+        Short max = null;
+        for (Short s : this) {
+            // EQMU: Changing the conditional boundary below produces an equivalent mutant.
+            if (s != null && (max == null || s > max)) {
+                max = s;
+            }
+        }
+        return max;
+    }
+
+    @Override
+    public Short min() throws IndexOutOfBoundsException {
+        if (size() == 0) {
+            throw new IndexOutOfBoundsException("Cannot return a minimum for an empty collection.");
+        }
+        Short min = null;
+        for (Short s : this) {
+            // EQMU: Changing the conditional boundary below produces an equivalent mutant.
+            if (s != null && (min == null || s < min)) {
+                min = s;
+            }
+        }
+        return min;
+    }
+
+    @Override
+    public Short product() {
+        Short product = 1;
+        for (Short i : this) {
+            if (i != null) {
+                product = (short) (product * i);
+            }
+        }
+        return product;
+    }
+
+    @Override
+    public Short sum() {
+        Short sum = 0;
+        for (Short i : this) {
+            if (i != null) {
+                sum = (short) (sum + i);
+            }
+        }
+        return sum;
+    }
+}
